f2e6d8f7ba04293c6896835941f3653926b80b75
manishsaraan <manish021js@gmail.com>
2018-07-08 22:50:46 +0530

    Middleware removed

--- server.js
+++ server.js
@@ -46,24 +46,7 @@ router.use(function(req, res, next) {
 	console.log('Something is happening.');
 	next();
 });
-app.use(function (req, res, next) {
 
-    // Website you wish to allow to connect
-	res.setHeader('Access-Control-Allow-Origin', 'https://invitetoslack.netlify.com/');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
 	res.json({ message: 'hooray! welcome to our api!' });
